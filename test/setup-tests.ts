9fabda93863f3a4ce4032690fdf48fb22a266c47
Heinous Tugboat <admin@heinous.industries>
2022-09-30 22:15:17 -0400

    Adds Mocha, Chai and Sinon testing utilities, as well as basic specs
    Closes #9

---
+++ test/setup-tests.ts
@@ -0,0 +1,31 @@
+import chai from 'chai';
+import { createSandbox, stub } from 'sinon';
+import sinonChai from 'sinon-chai';
+
+export async function mochaGlobalSetup() {
+  // initial global setup
+  // runs once for all threads
+}
+
+export const mochaHooks: Mocha.RootHookObject = {
+  // runs once at the beginning of each thread
+  beforeAll(done: () => void) {
+    done();
+  },
+  // runs once at the end of each thread
+  afterAll(done: () => void) {
+    done();
+  },
+  // runs once before each test
+  beforeEach(done: () => void) {
+    chai.should();
+    chai.use(sinonChai);
+    this.sandbox = createSandbox();
+    done();
+  },
+  // runs once after each test
+  afterEach(done: () => void) {
+    this.sandbox.restore();
+    done();
+  }
+}
