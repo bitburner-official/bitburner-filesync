9b255532af1478ed02820ecf3b24ffc590f6f320
Zoë Hoekstra <altlockg@gmail.com>
2022-10-10 00:33:37 +0200

    Add manual push/pull feature

--- src/networking/messageHandler.ts
+++ src/networking/messageHandler.ts
@@ -3,9 +3,30 @@ import { Stats, writeFile } from "fs";
 import { RawData } from "ws";
 import { config } from "../config";
 import { EventType } from "../eventTypes";
-import { fileChangeEventToMsg } from "./messageGenerators";
+import { fileChangeEventToMsg, requestAllFiles } from "./messageGenerators";
 import type { Signal } from "signal-js";
-import { Message } from "../interfaces";
+import { FileContent, Message } from "../interfaces";
+
+/*function deserialize(data: RawData): Message {
+  const msg = JSON.parse(data.toString());
+
+  if (typeof msg.jsonrpc !== "string" || msg.jsonrpc !== "2.0" || typeof msg.id !== "number") {
+    throw Error("Malformed data received.");
+  }
+
+  const id: number = msg.id;
+  let request = messageTracker.get(id);
+
+  console.log(msg);
+
+  if (msg.error != null) {
+    throw Error(msg.error);
+  } else if (msg.result == null) {
+    throw Error("Malformed JSON received.");
+  }
+
+  return { jsonrpc: "2.0", method: !request? msg.method : request.method, result: msg.result, id };
+}*/
 
 function deserialize(data: RawData): Message {
   const msg = JSON.parse(data.toString());
@@ -15,21 +36,24 @@ function deserialize(data: RawData): Message {
   }
 
   const id: number = msg.id;
-  const request = messageTracker.get(id);
+  const request = id === Number.MAX_SAFE_INTEGER ? { method: msg.method } : messageTracker.get(id);
 
-  if (typeof request?.method !== "string") {
-    throw Error("Malformed JSON received.");
-  } else if (msg.error != null) {
+  console.log("M:", msg);
+  console.log("R:", request);
+
+  if (msg.error != null) {
     throw Error(msg.error);
-  } else if (msg.result == null) {
-    throw Error("Malformed JSON received.");
   }
 
-  return { jsonrpc: "2.0", method: request.method, result: msg.result, id };
+  return { jsonrpc: "2.0", method: request?.method, result: msg.result, id };
 }
 
 export function isStringArray(s: Array<unknown>): s is string[] {
   return s.every((s) => typeof s === "string");
+}
+
+export function isFileContentArray(a: Array<unknown>): a is FileContent[] {
+  return a.every((f) => !!f && Object.hasOwn(f, "filename") && Object.hasOwn(f, "content"));
 }
 
 export function messageHandler(signaller: Signal, data: RawData, paths: Map<string, Stats>) {
@@ -49,9 +73,9 @@ export function messageHandler(signaller: Signal, data: RawData, paths: Map<stri
       writeFile(config.get("definitionFile").location, incoming.result, (err) => {
         if (err) return console.log(err);
       });
-
       break;
-    case "getFileNames": {
+
+    case "getFileNames":
       if (!Array.isArray(incoming.result) || !isStringArray(incoming.result))
         return console.log("Malformed data received.");
 
@@ -61,7 +85,30 @@ export function messageHandler(signaller: Signal, data: RawData, paths: Map<stri
         if (!stats.isDirectory() && !gameFiles.includes(fileName))
           signaller.emit(EventType.MessageSend, fileChangeEventToMsg({ path: fileName }));
       });
-    }
+      break;
+
+    case "getAllFiles":
+      if (!Array.isArray(incoming.result) || !isFileContentArray(incoming.result))
+        return console.log("Malformed data received.");
+
+      const gameFiles2 = incoming.result.map((f: FileContent) => {
+        return { filename: removeLeadingSlash(f.filename), content: f.content };
+      });
+
+      gameFiles2.forEach((f) => console.log(f));
+      break;
+
+    case "manualPullFromGame":
+      console.log("PULL");
+      signaller.emit(EventType.MessageSend, requestAllFiles());
+      break;
+
+    case "manualPushToGame":
+      console.log("PUSH");
+      paths.forEach((stats, fileName) => {
+        if (!stats.isDirectory()) signaller.emit(EventType.MessageSend, fileChangeEventToMsg({ path: fileName }));
+      });
+      break;
   }
 }
 
