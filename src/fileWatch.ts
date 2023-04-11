2f1807812d983b65c104d3c7333ebeaa27f6082b
david <davidchen1218@outlook.com>
2023-04-11 12:12:34 +1000

    Added folder exclusion feature and .gitattributes

--- src/fileWatch.ts
+++ src/fileWatch.ts
@@ -5,12 +5,7 @@ import { mkdir } from "fs/promises";
 import { resolve } from "path";
 import type { Signal } from "signal-js";
 import type { File } from "./interfaces";
-
-function fileFilter(file: File) {
-  if (config.get("allowedFiletypes").some((extension) => file.path.endsWith(extension))) return true;
-  if (file.stats.isDirectory()) return true;
-  return false;
-}
+import fs from "node:fs";
 
 function isError(err: unknown): err is NodeJS.ErrnoException {
   return (err as NodeJS.ErrnoException).code !== undefined;
@@ -25,6 +20,22 @@ export async function setupWatch(signaller: Signal) {
       process.exit();
     }
   }
+
+  const excludedPaths = config.get("exclude").map((path) => resolve(path));
+  const excludedFiles = new Set<string>();
+  for (const excludedPath of excludedPaths) {
+    for (const excludedFile of fs.readdirSync(excludedPath)) {
+      excludedFiles.add(excludedFile);
+    }
+  }
+
+  const fileFilter = (file: File) => {
+    // If the file is excluded, skip all other checks and ignore it.
+    if (excludedFiles.has(file.path)) return false;
+    if (config.get("allowedFiletypes").some((extension) => file.path.endsWith(extension))) return true;
+    if (file.stats.isDirectory()) return true;
+    return false;
+  };
 
   const watch = new CheapWatch({
     dir: config.get("scriptsFolder"),
